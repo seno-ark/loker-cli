24a2af4849c3606a5cc2a4a22c0440ccc8604525
seno <seno@deb>
2020-03-28 16:19:28 +0700

    create loker app

---
+++ loker.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+	"strings"
+	"regexp"
+)
+
+func createLoker(capacity int) (res string, err error){
+	if capacity < 1 {
+		err = errors.New(message["invalid_argument"])
+		return
+	}
+
+	if len(loker) > 0 {
+		err = errors.New(message["init_duplicate"])
+		return	
+	}
+
+	// slice berisi element berupa zero value idCard
+	loker = make([]idCard, capacity)
+
+	res = fmt.Sprintf(message["init_success"], len(loker))
+	return
+}
+
+func lokerStatus() (res string, err error) {
+
+	if len(loker) == 0 {
+		err = errors.New(message["loker_slot_empty"])
+		return
+	}
+
+	tableRows := []string{
+		"============================================",
+		"No Loker\tTipe Identitas\tNo Identitas",
+		"--------------------------------------------",
+	}
+
+	for i, card := range loker {
+		if card != (idCard{}) {
+			tableRows = append(tableRows, fmt.Sprintf("%d\t\t%s\t\t%d", i+1, card.Type, card.No))
+		}
+	}
+
+	res = strings.Join(tableRows, "\n")
+	return
+}
+
+func inputIdCard(cardType string, cardNo int) (res string, err error){
+
+	regex, _ := regexp.Compile(`^[a-zA-Z]+$`)
+	cardTypeIsAlphabet := regex.MatchString(cardType)
+
+	if cardType == "" || cardNo < 1 || !cardTypeIsAlphabet{
+		err = errors.New(message["invalid_argument"])
+		return
+	}
+
+	if len(loker) == 0 {
+		err = errors.New(message["loker_slot_empty"])
+		return
+	}
+
+	// check existing idCard, prevent duplicate idCard Num
+	for i, v := range loker {
+		if v.No == cardNo {
+			err = errors.New(fmt.Sprintf(message["input_duplicate"], i+1))
+			return
+		}
+	}
+
+	cardType = strings.ToUpper(cardType)
+
+	lokerId := 0
+	for i, v := range loker {
+		if v == (idCard{}) {
+			loker[i] = idCard{cardType, cardNo}
+			lokerId = i+1
+			break
+		}
+	}
+
+	if lokerId == 0 {
+		err = errors.New(message["input_failed"])
+		return
+	}
+
+	res = fmt.Sprintf(message["input_success"], lokerId)
+	return
+}
+
+func leaveIdCard(lokerId int) (res string, err error){
+
+	if lokerId < 1 {
+		err = errors.New(message["invalid_argument"])
+		return
+	}
+
+	if len(loker) == 0 {
+		err = errors.New(message["loker_slot_empty"])
+		return
+	}
+
+	if lokerId > len(loker) {
+		// nomor loker tidak tersedia
+		err = errors.New(fmt.Sprintf(message["leave_failed"], lokerId))
+		return
+	}
+	if loker[lokerId-1] == (idCard{}) {
+		// nomor loker tersedia tapi kosong
+		err = errors.New(fmt.Sprintf(message["leave_empty"], lokerId))
+		return
+	}
+
+	loker[lokerId-1] = idCard{}
+
+	res = fmt.Sprintf(message["leave_success"], lokerId)
+	return
+}
+
+func findIdCard(cardNo int) (res string, err error){
+
+	if len(loker) == 0 {
+		err = errors.New(message["loker_slot_empty"])
+		return
+	}
+
+	for i, card := range loker {
+		if card.No == cardNo {
+			res = fmt.Sprintf(message["find_result"], i+1)
+			return
+		}
+	}
+	res = message["find_empty"]
+	return
+}
+
+func searchIdCard(cardType string) (res string, err error){
+
+	if len(loker) == 0 {
+		err = errors.New(message["loker_slot_empty"])
+		return
+	}
+
+	cardType = strings.ToUpper(cardType)
+
+	var searchResults []string
+	for _, card := range loker {
+		if card.Type == cardType {
+			searchResults = append(searchResults, fmt.Sprintf("%d", card.No))
+		}
+	}
+
+	if len(searchResults) == 0 {
+		err = errors.New(fmt.Sprintf(message["search_empty"], cardType))
+		return
+	}
+
+	res = fmt.Sprintf("No Identitas: %s", strings.Join(searchResults, ", "))
+	return
+}
---
+++ main.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+	"errors"
+)
+
+type idCard struct {
+	Type string
+	No int
+}
+var loker []idCard
+
+func initHandler(capacity string) {
+	// Fungsi untuk menangani perintah `init`
+
+	var res string
+	var err error
+	var lokerCap int
+
+	if capacity == "" {
+		err = errors.New(message["empty_argument"])
+		showResult(res, err)
+		return
+	}
+
+	lokerCap, err = strconv.Atoi(capacity)
+	if err != nil {
+		err = errors.New(message["invalid_argument"])
+		showResult(res, err)
+		return
+	}
+
+	res, err = createLoker(lokerCap)
+	showResult(res, err)
+}
+
+func statusHandler() {
+	// Fungsi untuk menangani perintah `status`
+
+	res, err := lokerStatus()
+	showResult(res, err)
+}
+
+func inputHandler(cardType, cardNo string) {
+	// Fungsi untuk menangani perintah `input`
+
+	var res string
+	var err error
+	var cardNumber int
+
+	if cardType == "" || cardNo == "" {
+		err = errors.New(message["empty_argument"])
+		showResult(res, err)
+		return
+	}
+
+	cardNumber, err = strconv.Atoi(cardNo)
+	if err != nil {
+		err = errors.New(message["invalid_argument"])
+		showResult(res, err)
+		return
+	}
+
+	res, err = inputIdCard(cardType, cardNumber)
+	showResult(res, err)
+}
+
+func leaveHandler(lokerNo string) {
+	// Fungsi untuk menangani perintah `leave`
+
+	var res string
+	var err error
+	var lokerId int
+
+	if lokerNo == "" {
+		err = errors.New(message["empty_argument"])
+		showResult(res, err)
+		return
+	}
+
+	lokerId, err = strconv.Atoi(lokerNo)
+	if err != nil {
+		err = errors.New(message["invalid_argument"])
+		showResult(res, err)
+		return
+	}
+
+	res, err = leaveIdCard(lokerId)
+	showResult(res, err)
+}
+
+func findHandler(cardNo string) {
+	// Fungsi untuk menangani perintah `find`
+
+	var res string
+	var err error
+	var cardNumber int
+
+	if cardNo == "" {
+		err = errors.New(message["empty_argument"])
+		showResult(res, err)
+		return
+	}
+
+	cardNumber, err = strconv.Atoi(cardNo)
+	if err != nil {
+		err = errors.New(message["invalid_argument"])
+		showResult(res, err)
+		return
+	}
+
+	res, err = findIdCard(cardNumber)
+	showResult(res, err)
+}
+
+func searchHandler(cardType string) {
+	// Fungsi untuk menangani perintah `search`
+	
+	var res string
+	var err error
+
+	if cardType == "" {
+		err = errors.New(message["empty_argument"])
+		showResult(res, err)
+		return
+	}
+
+	res, err = searchIdCard(cardType)
+	showResult(res, err)
+}
+
+func process(userInput []string) bool {
+	// Fungsi untuk memroses inputan user
+
+	command, arg1, arg2 := unpackInput(userInput)
+	command = strings.ToLower(command)
+	var err error
+
+	switch command {
+
+		case "init":
+			initHandler(arg1)
+		case "status":
+			statusHandler()
+		case "input":
+			inputHandler(arg1, arg2)
+		case "leave":
+			leaveHandler(arg1)
+		case "find":
+			findHandler(arg1)
+		case "search":
+			searchHandler(arg1)
+		case "exit":
+			return false
+		case "":
+			err = errors.New(message["empty_command"])
+			showResult("", err)
+		default:
+			err = errors.New(message["invalid_command"])
+			showResult("", err)
+	}
+
+	return true
+}
+
+func main() {	
+
+	var userInput []string
+	fmt.Println(message["welcome"])
+
+	i := 0
+	for {
+		userInput = getInput()
+		if !process(userInput) {
+			break
+		}
+		i++
+	}
+	fmt.Println(message["exit"])
+}
